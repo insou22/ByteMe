9bdb4e468e8a47a93b0c33e7b1b841d8fd070bc1
insou22 <zac.kologlu@gmail.com>
2018-05-10 18:56:20 +1000

    Main simplification

--- src/co/insou/byteme/Main.java
+++ src/co/insou/byteme/Main.java
@@ -4,29 +4,76 @@ import co.insou.byteme.vm.ByteMeExecutionResult;
 import co.insou.byteme.vm.ByteMeProgram;
 import co.insou.byteme.vm.ByteMeVM;
 
+import java.io.File;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public final class Main {
 
     public static void main(String[] args) throws Exception
     {
-        if (args.length == 0)
-        {
-            System.out.println("Usage: java -jar ByteMe.jar <file.bm>");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            Path path;
+            System.out.print("Demo example? ");
+
+            boolean demo = scanner.nextBoolean();
+            scanner.nextLine();
+
+            if (demo) {
+                System.out.println("Example options: ");
+                for (File example : Objects.requireNonNull(Paths.get("examples").toFile().listFiles())) {
+                    System.out.println("  " + example.getName().substring(0, example.getName().length() - 3));
+                }
+                System.out.print("Example name? ");
+                path = Paths.get("examples/" + scanner.nextLine() + ".bm");
+            } else {
+                System.out.println("Path: ");
+                path = Paths.get(scanner.nextLine());
+            }
+
+            Main.printDoubleLine();
+
+            ByteMeVM vm = ByteMe.defaultVM();
+
+            ByteMeProgram program = vm.compile(
+                    Files.readAllLines(path)
+            );
+
+            Main.printPaddedBorder();
+
+            ByteMeExecutionResult result = vm.execute(program);
+
+            Main.printPaddedBorder();
+            System.out.println("Result: " + result.resultType());
+            System.out.println("Data: " + Arrays.toString(result.data()));
+            Main.printDoubleLine();
         }
+    }
 
-        ByteMeVM vm = ByteMe.defaultVM();
+    private static void printPaddedBorder() {
+        Main.printBlankLine();
+        Main.printBorder();
+        Main.printBlankLine();
+    }
 
-        ByteMeProgram program = vm.compile(
-                Files.readAllLines(Paths.get(args[0]))
-        );
+    private static void printBorder() {
+        System.out.println(IntStream.range(0, 50).mapToObj(i -> "=").collect(Collectors.joining()));
+    }
 
-        ByteMeExecutionResult result = vm.execute(program);
+    private static void printBlankLine() {
+        System.out.println();
+    }
 
-        System.out.println("Result: " + result.resultType());
-        System.out.println("Data: " + Arrays.toString(result.data()));
+    private static void printDoubleLine() {
+        System.out.println("\r\n");
     }
 
 }
